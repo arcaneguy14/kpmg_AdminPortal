359948b36f91672a297ca0336836fd0c5022caeb
prashan <prashan@sage42.net>
2019-03-25 18:07:56 +0800

    Social Media Linkage on Edit User

--- AdminPortal/src/views/users/EditUser.vue
+++ AdminPortal/src/views/users/EditUser.vue
@@ -1,70 +1,94 @@
 <template>
-  <b-form @submit="onSubmit" @reset="onReset" v-if="show" id="modalInfo">
-    <b-row>
-      <b-col sm="6">
-        <b-form-group id="exampleInputGroup2" label="Your Name:" label-for="exampleInput2">
-          <b-form-input
-            id="exampleInput2"
-            type=""
-            :value="JSON.parse(modalInfo.content).name"
-            required
-            placeholder="Enter name" />
-        </b-form-group>
-      </b-col>
+  <b-tabs content-class="mt-3">
+    <b-tab title="User Detail" active>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show" id="modalInfo">
+        <b-row>
+          <b-col sm="6">
+            <b-form-group id="exampleInputGroup2" label="Your Name:" label-for="exampleInput2">
+              <b-form-input
+                id="exampleInput2"
+                type=""
+                :value="JSON.parse(modalInfo.content).name"
+                required
+                placeholder="Enter name" />
+            </b-form-group>
+          </b-col>
 
-      <b-col sm="6">
-        <b-form-group
-          id="exampleInputGroup1"
-          label="Email address:"
-          label-for="exampleInput1"
-          description="We'll never share your email with anyone else."
-        >
-          <b-form-input
-            id="exampleInput1"
-            type="email"
-            :value="JSON.parse(modalInfo.content).email"
-            required
-            placeholder="Enter email" />
-        </b-form-group>
-      </b-col>
-    </b-row>
+          <b-col sm="6">
+            <b-form-group
+              id="exampleInputGroup1"
+              label="Email address:"
+              label-for="exampleInput1"
+              description="We'll never share your email with anyone else."
+            >
+              <b-form-input
+                id="exampleInput1"
+                type="email"
+                :value="JSON.parse(modalInfo.content).email"
+                required
+                placeholder="Enter email" />
+            </b-form-group>
+          </b-col>
+        </b-row>
 
-    <b-row>
-      <b-col>
-        <b-form-group
-          id="exampleInputGroup1"
-          label="Address:"
-          label-for="exampleInput1"
-        >
-          <b-form-textarea
-            id="textarea"
-            v-model="form.address"
-            required
-            placeholder="Enter Address..."
-            rows="3"
-            max-rows="6"
-          />
-        </b-form-group>
-      </b-col>
-      <b-col>
-        <b-form-group id="exampleInputGroup3" label="Nationality:" label-for="exampleInput3">
-          <b-form-select id="exampleInput3" :options="countries" required v-model="form.country" />
-        </b-form-group>
-      </b-col>
-    </b-row>
+        <b-row>
+          <b-col>
+            <b-form-group
+              id="exampleInputGroup1"
+              label="Address:"
+              label-for="exampleInput1"
+            >
+              <b-form-textarea
+                id="textarea"
+                v-model="form.address"
+                required
+                placeholder="Enter Address..."
+                rows="3"
+                max-rows="6"
+              />
+            </b-form-group>
+          </b-col>
+          <b-col>
+            <b-form-group id="exampleInputGroup3" label="Nationality:" label-for="exampleInput3">
+              <b-form-select id="exampleInput3" :options="countries" required v-model="form.country" />
+            </b-form-group>
+          </b-col>
+        </b-row>
 
-    <b-row>
-      <b-col>
-        <b-form-group id="exampleGroup4" label="Gender">
-          <b-form-radio-group v-model="form.checked" id="exampleChecks">
-            <b-form-radio value="m">Male</b-form-radio>
-            <b-form-radio value="f">Female</b-form-radio>
-          </b-form-radio-group>
+        <b-row>
+          <b-col>
+            <b-form-group id="exampleGroup4" label="Gender">
+              <b-form-radio-group v-model="form.checked" id="exampleChecks">
+                <b-form-radio value="m">Male</b-form-radio>
+                <b-form-radio value="f">Female</b-form-radio>
+              </b-form-radio-group>
+            </b-form-group>
+          </b-col>
+        </b-row>
+
+        <b-form-group id="genPwd" label="" label-for="genPass">
+          <b-row>
+            <b-col>
+              <b-form-input
+                id="genPass"
+                type="text"
+                v-model="form.pwd"
+                required
+                readonly
+                placeholder="" />
+            </b-col>
+
+            <b-col>
+              <b-button type="button" @click="showAlert" variant="primary">Generate Password</b-button>
+            </b-col>
+          </b-row>
         </b-form-group>
-      </b-col>
 
-
-      <b-col>
+        <b-button type="submit" variant="primary" class="mr-1">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-tab>
+    <b-tab title="Social Media Linking">
         <b-form-group id="exampleInputGroup2" label="Social Media Linking" label-for="social-media">
           <b-row>
             <b-col sm="6">
@@ -109,30 +133,8 @@
             </b-col>
           </b-row>
         </b-form-group>
-      </b-col>
-    </b-row>
-
-    <b-form-group id="genPwd" label="" label-for="genPass">
-      <b-row>
-        <b-col>
-          <b-form-input
-            id="genPass"
-            type="text"
-            v-model="form.pwd"
-            required
-            readonly
-            placeholder="" />
-        </b-col>
-
-        <b-col>
-          <b-button type="button" @click="showAlert" variant="primary">Generate Password</b-button>
-        </b-col>
-      </b-row>
-    </b-form-group>
-
-    <b-button type="submit" variant="primary" class="mr-1">Submit</b-button>
-    <b-button type="reset" variant="danger">Reset</b-button>
-  </b-form>
+    </b-tab>
+  </b-tabs>
 </template>
 
 <script>
@@ -223,14 +225,10 @@
       },
 
       active(arr){
-        if (arr == 1)
-        this.isActive1 = true;
-        if (arr == 2)
-          this.isActive2 = true;
-        if (arr == 3)
-          this.isActive3 = true;
-        if (arr == 4)
-          this.isActive4 = true;
+        this.isActive1 = arr == 1;
+        this.isActive2 = arr == 2;
+        this.isActive3 = arr == 3;
+        this.isActive4 = arr == 4;
       },
 
       notActive(arr){
